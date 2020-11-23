1d227f3efb0d80d9d2d2bcaeb6ac4e82ec3b283c
Kristijan Susnjara <kristijan.susnjara@outlook.com>
2020-11-24 00:49:14 +0100

    Add GameKey.h

--- gamekey.h
+++ gamekey.h
@@ -3,5 +3,16 @@
 
 #include "player.h"
 
+using namespace std;
+//Uebernommen von Folien
+
+class Game;
+class GameKey{
+	GameKey() {}	//Private. Moze implementirano bit i u GameKey.cpp
+	friend bool Player::joing_game(shared_ptr<Game>);
+	friend bool Player::leave_game(shared_ptr<Game>);
+
+};
+
 
 #endif
