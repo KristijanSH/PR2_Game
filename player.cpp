c376785e415765a31c8126b32937c8cc2991d5e5
Kristijan Susnjara <kristijan.susnjara@outlook.com>
2020-12-05 22:31:10 +0100

    update leave_game

--- player.cpp
+++ player.cpp
@@ -89,7 +89,7 @@ bool Player::host_game(string s, Mode m)
 }
 */
 	bool Player::join_game(shared_ptr<Game> g) {
-		string tmpName = g->get.name();
+/*		string tmpName = g->get_name();
 		auto it = games.find(tmpName);
 		if(it == games.end()) {
 			games.insert({tmpName, g});
@@ -98,17 +98,36 @@ bool Player::host_game(string s, Mode m)
 			return true;
 		}
 		return false;
+*/    
+  /*
+  auto findP = find_if(g->get_players().begin(), g->get_players().end(), [&thisname](const pair<string, shared_ptr<Player>>& val) {
+        return val.second->get_name() == thisname;
+    });
+if (findP != g->get_players().end()) return false;
+  */
+    
 	}
 	bool Player::leave_game(shared_ptr<Game> g) {
-		string currentG = g.get_name();
-		if(games.find(currentG)) {
-			games.erase(currentG);
-			delete this->g;
-			return true;
-		} else
-			return false;
+    // g aus der map loeschen, und auch this aus g.players loeschen
+    //remove_if bei games und dann bei players
+    string gn = g->get_name();
+    auto res = remove_if(games.begin(), games.end(), [&gn](const pair <string, weak_ptr<Game>>& ga)
+        {
+            return gn == ga.first;
+        });
+
+    if (res == games.end()) return false;
+    games.erase(res, games.end());
+    auto p = shared_from_this();
+    GameKey gk;
+    bool pl = g->remove_player(gk, p);
+    
+    if (pl) return true;
+    
+    return false;
 
 	}
+
 	vector<weak_ptr<Player>> Player::invite_players(const vector<weak_ptr<Player>>& v) {
 		vector<weak_ptr<Player>> inviteListe;
 
